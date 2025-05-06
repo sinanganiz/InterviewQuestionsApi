a05def862a2f999bd6bff7529d74d9403e25d7e4
Sinan Ganiz <sinan.ganiz@gmail.com>
2025-05-06 17:41:38 +0300

    Initial solution setup with layered architecture

---
+++ src/InterviewQuestionsApi.Presentation/Program.cs
@@ -0,0 +1,22 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddOpenApi();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapControllers();
+app.UseHttpsRedirection();
+
+app.Run();
